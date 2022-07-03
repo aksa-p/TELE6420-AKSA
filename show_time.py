7bdaabcce41684c51b1b0f009a403e5aae75b473
Aksa Mary Pramod <pramod.a@northeastern.edu>
2022-07-03 18:29:36 -0400

    dev1 adding time an date

--- show_time.py
+++ show_time.py
@@ -2,3 +2,4 @@ from datetime import datetime
 time_now = datetime.now()
 time_current = time_now.strftime("%H:%M:%S")
 print("The current time is ", time_current)
+print("The current date is", time_now.date())
