814e7f9ad586ecb97ce427913613b849524c0a94
Aksa Mary Pramod <pramod.a@northeastern.edu>
2022-07-03 19:02:45 -0400

    dev2 adding in time, date and day

--- show_time.py
+++ show_time.py
@@ -1,4 +1,10 @@
 from datetime import datetime
+from datetime import date
+import calendar
 time_now = datetime.now()
 time_current = time_now.strftime("%H:%M:%S")
+date_c = date.today()
+
 print("The current time is ", time_current)
+print("The current date is", time_now.date())
+print(calendar.day_name[date_c.weekday()])
